df40f3b4b20a18f89e266177b46d7edf1c32115c
manchandajayant <maanjayant@gmail.com>
2020-03-11 13:39:24 +0100

    endpoints added for adding albums and getting  them

--- Albums/router.js
+++ Albums/router.js
@@ -6,10 +6,20 @@ const router = new Router();
 router.get("/albums", async (req, res, next) => {
   try {
     const album = await Album.findAll({
-      attributes: ["title", "artist", "url"],
+      attributes: ["title", "Artist", "url"],
       raw: true
     });
     res.send(album);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/albums", async (req, res, next) => {
+  try {
+    const createAlbum = await Album.create(req.body);
+
+    res.json(createAlbum);
   } catch (error) {
     next(error);
   }
